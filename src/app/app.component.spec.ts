c856380780bbee1b0203224bb926e3d0e63de16d
aghiljv <aghiljv@gmail.com>
2023-04-02 23:08:08 +0200

    minor changes for title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tasks'`, () => {
+  it(`should have as title 'repeater-task'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tasks');
+    expect(app.title).toEqual('repeater-task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tasks app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'repeater-task app is running!'
+    );
   });
 });
